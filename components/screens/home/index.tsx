a57202749de1f51453d16430ccc1ed260794756a
DamiaoFilho <damiaofilho0123@gmail.com>
2024-03-20 23:17:53 -0300

    some tests

---
+++ components/screens/home/index.tsx
@@ -0,0 +1,8 @@
+import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import { style } from './style'
+
+export default function Home(){
+    return(
+        <Text style={ style.body }>Hello World!</Text>
+    )
+}
